0e922a366dab65e4898b1769433cb923ac75865c
IlyaNikolaevitch <mister.bin1981@gmail.com>
2024-09-17 21:37:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ name = 'Python,'
 kollichestvo_zadaniy = 12
 kollitchestvo_chasov = 1.5
 wremya_na_odno_zadanie = (1.5 / 12)
-print('Курс:', name, 'всего задач:', kollichestvo_zadaniy, 'затрачено часов:', kollitchestvo_chasov, 'время выполнения', wremya_na_odno_zadanie, 'часа.')
+print('Курс:', name, 'всего задач:', kollichestvo_zadaniy,',''затрачено часов:', kollitchestvo_chasov,',' 'время выполнения', wremya_na_odno_zadanie, 'часа.')
