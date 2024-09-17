4470ddb9d75c75449490d688d1dd7d5f0d5e3297
IlyaNikolaevitch <mister.bin1981@gmail.com>
2024-09-17 21:17:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 name = 'Python,'
-kollichestvo_zadaniy = '12,'
-kollitchestvo_chasov = '1.5,'
+kollichestvo_zadaniy = 12
+kollitchestvo_chasov = 1.5
 wremya_na_odno_zadanie = (1.5 / 12)
 print('Курс:', name, 'всего задач:', kollichestvo_zadaniy, 'затрачено часов:', kollitchestvo_chasov, 'время выполнения', wremya_na_odno_zadanie, 'часа.')
